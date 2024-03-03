4a8e07b59d09524e43c85f2a2c071b142942d9c6
Sharon Grace T. Hangaan <sharonygracehangaan@gmail.com>
2024-03-03 16:13:49 +0800

    update ActiveState.java

--- ActiveState.java
+++ ActiveState.java
@@ -1,33 +1,34 @@
 public class ActiveState implements AccountState {
 
- 
+    // suspend or close
+
     @Override
     public void activate(Account account) {
-        System.out.println("Account is already activated!");
+        System.out.println("\tAccount is already activated!");
     }
 
     @Override
     public void suspend(Account account) {
-        System.out.println("Account is suspended!");
+        System.out.println("\tAccount is suspended!");
         account.setAccountState(new SuspendedState());
     }
 
     @Override
     public void close(Account account) {
-        System.out.println("Account is closed!");
+        System.out.println("\tAccount is closed!");
         account.setAccountState(new ClosedState());
     }
 
     @Override
     public void withdraw(Double withdrawAmount, Account account) {
-        System.out.println("Withdrawing " + withdrawAmount);
+        System.out.println("\tWithdrawing " + withdrawAmount);
         account.setBalance(account.getBalance() - withdrawAmount);
         System.out.println(account);
     }
 
     @Override
     public void deposit(Double depositAmount, Account account) {
-        System.out.println("Depositing " + depositAmount);
+        System.out.println("\tDepositing " + depositAmount);
         account.setBalance(account.getBalance() + depositAmount);
         System.out.println(account);
     }
