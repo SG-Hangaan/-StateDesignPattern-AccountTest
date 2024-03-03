68ca0ddba06956e5c590397f55c63d7a85090928
Sharon Grace T. Hangaan <sharonygracehangaan@gmail.com>
2024-03-03 16:14:08 +0800

    update ClosedState.java

--- ClosedState.java
+++ ClosedState.java
@@ -2,28 +2,28 @@ public class ClosedState implements AccountState {
  
     @Override
     public void activate(Account account) {
-        System.out.println("You cannot activate a closed account!");
+        System.out.println("\tYou cannot activate a closed account!");
     }
 
     @Override
     public void suspend(Account account) {
-        System.out.println("You cannot suspend a closed account!");
+        System.out.println("\tYou cannot suspend a closed account!");
     }
 
     @Override
     public void close(Account account) {
-        System.out.println("Account is already closed!");
+        System.out.println("\tAccount is already closed!");
     }
 
     @Override
     public void withdraw(Double withdrawAmount, Account account) {
-        System.out.println("You cannot withdraw on a closed account!");
+        System.out.println("\tYou cannot withdraw on a closed account!");
         System.out.println(account);
     }
 
     @Override
     public void deposit(Double depositAmount, Account account) {
-        System.out.println("You cannot withdraw on a closed account!");
+        System.out.println("\tYou cannot withdraw on a closed account!");
         System.out.println(account);
     }
 }
